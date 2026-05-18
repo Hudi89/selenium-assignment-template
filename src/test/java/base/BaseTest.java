0e23a2d12aa829591fedb8905b02b5d08eb80a1c
AkylbekkyzyAsel <akylbek.asel2001@gmail.com>
2026-05-18 23:35:49 +0200

    fix BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,9 +3,12 @@ package base;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import utils.ConfigReader;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 
@@ -13,13 +16,13 @@ public class BaseTest {
     private static final String BASE_URL = ConfigReader.get("base.url");
 
     @Before
-    public void setup() {
+    public void setup() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
         options.setAcceptInsecureCerts(true);
         options.addArguments("--headless=new");
         options.addArguments("--window-size=1920,1080");
 
-        driver = new ChromeDriver(options);
+        driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
         driver.manage().window().maximize();
         driver.get(BASE_URL);
     }
